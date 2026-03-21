d8535d1494f1fced973d1077022d45414736b0bd
Antonio Blanca Duarte <antonioblancaduarte2@gmail.com>
2026-03-21 17:27:57 +0100

    Creación de la tabla

--- app/src/main/java/com/antonio/taskmaster/data/Task.kt
+++ app/src/main/java/com/antonio/taskmaster/data/Task.kt
@@ -1,3 +1,11 @@
 package com.antonio.taskmaster.data
 
-data class Task()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String,
+    val isDone: Boolean = false
+)
